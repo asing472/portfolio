9a4d51a204845141183b5df33a3cddb2dfa4abbc
Akash Kumar Singh <akash18011997@gmail.com>
2024-12-07 03:13:59 +0530

    deploy take 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  // base: "https://asing472.github.io/Website/"
+  plugins: [react()],
+  base: "/Website/"
 })
