ac0e789338b780b01f3667aaba73e156d321ed8f
Akash Kumar Singh <akash18011997@gmail.com>
2024-12-07 03:31:56 +0530

    size limit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Website/"
+  base: "/Website/",
+  build: {
+    chunkSizeWarningLimit: 30000000, // Set the limit to 1000 KB (default is 500 KB)
+  },
 })
