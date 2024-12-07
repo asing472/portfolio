111b8b038a7d6f2118963c60988cb37a9e385cf7
Akash Kumar Singh <akash18011997@gmail.com>
2024-12-07 05:39:06 +0530

    fixed localhost errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* Wrap your app with BrowserRouter and set basename */}
-    <BrowserRouter basename="/Website"> {/* Replace 'Website' with your repo name */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
