40fdd06142b0bcab2b4e5a3ce79c026415d5f68a
Akash Kumar Singh <akash18011997@gmail.com>
2024-12-08 12:51:45 +0530

    edited vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
   outDir: 'dist',
   build: {
     chunkSizeWarningLimit: 30000000, // Set the limit to 1000 KB (default is 500 KB)
